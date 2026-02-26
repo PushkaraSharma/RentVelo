10e66431bc9473b780234cc1d4e9f2fe84f562f5
Pushkara Sharma <pushkarasharma11@gmail.com>
2026-02-26 13:58:08 +0530

    misc

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   return {
     "name": appName,
     "slug": "rentvelo",
-    "version": "1.0.0",
+    "version": "0.0.2",
     "orientation": "portrait",
     "icon": "./assets/app-icon.png",
     "userInterfaceStyle": "automatic",
