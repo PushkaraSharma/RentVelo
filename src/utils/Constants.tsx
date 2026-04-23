3c853453b34bc1de4faa294e118f5f45bdb5d66e
Pushkara Sharma <pushkarasharma11@gmail.com>
2026-04-23 15:45:08 +0530

    misc

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -69,14 +69,14 @@ export const GUEST_COUNTS = [
     '1 Person', '2 People', '3 People', '4 People', '5+ People'
 ];
 
-export const OTA_VERSION = 1;
+export const OTA_VERSION = 2;
 
 export const CHANGELOG = {
     version: `${Constants.expoConfig?.version}_${OTA_VERSION}`,
     features: [
-        "Added Rent Increment feature",
-        "Resolve receipt related issue",
-        "Bug fixes",
-        "Added Notification feature"
+        "Added Tenant and Room statistics",
+        "Resolve major issues",
+        "Ability to zoom images",
+        "Restricting access to any future rent cards"
     ]
 };
